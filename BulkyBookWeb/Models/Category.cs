4ed04d2a53859be6a5e46f912683474badc2d664
ALI ASHAD ULLAH <ALI ASHAD ULLAH@ALI-ASHAD-ULLAH>
2022-07-24 16:12:49 +0600

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order mst be between 1 to 100")]
+        public int DisplayOrder { get; set; }
+        [BindProperty, DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
